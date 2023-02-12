9b7a764ecb9539c1e214c40f251c166e68a33f70
Alex <alexis.autres@gmail.com>
2023-02-12 15:57:31 +0100

    Add README.md

--- CustomTable.tsx
+++ CustomTable.tsx
@@ -7,6 +7,7 @@ interface Props {
   pagination?: boolean;
   search?: boolean;
   sortable?: boolean;
+  filterEntries?: boolean;
 }
 
 const CustomTable: React.FC<Props> = (props) => {
@@ -16,9 +17,12 @@ const CustomTable: React.FC<Props> = (props) => {
     pagination = true,
     search = true,
     sortable = true,
+    filterEntries = true,
   } = props;
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+
   const [searchTerm, setSearchTerm] = useState("");
   const [sortColumn, setSortColumn] = useState<keyof Props["data"][0] | null>(
     null
@@ -27,24 +31,31 @@ const CustomTable: React.FC<Props> = (props) => {
     null
   );
 
-  const itemsPerPage = 10;
+  // Define a constant named `filteredData` that contains the filtered data from `data`
   const filteredData = data.filter((row) =>
+    // Check if any of the values in the `columns` array meet the search criteria
     columns.some((column) =>
+      // Check if the value of the current `row` at the `column.key` property, converted to a string and lowercased, includes the lowercased `searchTerm`
       row[column.key]
         .toString()
         .toLowerCase()
         .includes(searchTerm.toLowerCase())
     )
   );
+
+  // Define a constant named `sortedData` that is the result of sorting `filteredData` based on `sortColumn` and `sortDirection`
   const sortedData = sortable
     ? filteredData.sort((a, b) => {
+        // Check if `sortDirection` is equal to "asc"
         if (sortDirection === "asc") {
+          // If true, sort the data in ascending order
           return a[sortColumn!] < b[sortColumn!]
             ? -1
             : a[sortColumn!] > b[sortColumn!]
             ? 1
             : 0;
         } else {
+          // If false, sort the data in descending order
           return a[sortColumn!] > b[sortColumn!]
             ? -1
             : a[sortColumn!] < b[sortColumn!]
@@ -53,7 +64,9 @@ const CustomTable: React.FC<Props> = (props) => {
         }
       })
     : filteredData;
+
   const pages = Math.ceil(filteredData.length / itemsPerPage);
+
   const currentData = sortedData.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
@@ -63,32 +76,53 @@ const CustomTable: React.FC<Props> = (props) => {
     setCurrentPage(page);
   };
 
+  // Define a constant named `handleSearch` that takes in a parameter `searchTerm` of type `string`
   const handleSearch = (searchTerm: string) => {
+    // Set the value of `searchTerm` to the passed in `searchTerm`
     setSearchTerm(searchTerm);
+    // Set the value of `currentPage` to 1
     setCurrentPage(1);
   };
 
+  // Define a constant named `handleSort` that takes in a parameter `columnKey` of type `keyof Props["data"][0]`
   const handleSort = (columnKey: keyof Props["data"][0]) => {
+    // Check if `sortColumn` is equal to `columnKey`
     if (sortColumn === columnKey) {
+      // If true, toggle the value of `sortDirection` from "asc" to "desc" or vice versa
       setSortDirection(sortDirection === "asc" ? "desc" : "asc");
     } else {
+      // If false, set `sortColumn` to `columnKey`
       setSortColumn(columnKey);
+      // Set `sortDirection` to "asc"
       setSortDirection("asc");
     }
   };
 
   return (
     <div className="custom-table">
-      {search && (
-        <div className="custom-table-search">
+      <div className="custom-table-search">
+        {filterEntries && (
+          <select
+            value={itemsPerPage}
+            onChange={(e) => setItemsPerPage(Number(e.target.value))}
+          >
+            <option value={10}> 10 entries</option>
+            <option value={25}> Show 25 entries</option>
+            <option value={50}> Show 50 entries</option>
+            <option value={100}> Show 100 entries</option>
+          </select>
+        )}
+
+        {search && (
           <input
             type="text"
             placeholder="Search..."
             value={searchTerm}
             onChange={(e) => handleSearch(e.target.value)}
           />
-        </div>
-      )}
+        )}
+      </div>
+
       <table className="custom-table-container">
         <thead className="custom-table-header">
           <tr>
@@ -122,6 +156,12 @@ const CustomTable: React.FC<Props> = (props) => {
       </table>
       {pagination && (
         <div className="custom-table-pagination">
+          <span className="custom-table-pagination-info">
+            Showing {(currentPage - 1) * itemsPerPage + 1} to{" "}
+            {Math.min(currentPage * itemsPerPage, filteredData.length)} of{" "}
+            {filteredData.length} entries
+          </span>
+
           <button
             className="custom-table-pagination-button"
             disabled={currentPage === 1}
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# custom-react-table
-# custom-react-table
+### Features
+A custom table component built with React and TypeScript that allows for sorting, filtering, searching, and pagination of data.
+
+## Installation
+To install the dependencies, run: `npm install`
+
+## Usage
+In your React component, import the CustomTable component and pass in the required columns and data props.
+
+```react
+import React from 'react';
+import CustomTable from 'custom-table';
+
+const columns = [
+  { header: 'Name', key: 'name' },
+  { header: 'Email', key: 'email' },
+  { header: 'Phone', key: 'phone' },
+];
+
+const data = [
+  { name: 'John Doe', email: 'johndoe@example.com', phone: '555-555-5555' },
+  { name: 'Jane Doe', email: 'janedoe@example.com', phone: '555-555-5556' },
+  { name: 'Bob Smith', email: 'bobsmith@example.com', phone: '555-555-5557' },
+];
+
+const Example = () => (
+	<CustomTable 
+		data={data} 
+		columns={columns} 
+		pagination={true} 
+		search={true} 
+		sortable={true} 
+		filterEntries={true}
+	/>
+);
+
+export default Example;
+```
+In the example above, the columns array defines the headers and keys for each column in the table, and the data array provides the data to be displayed in the table.
+
+## Props
+- `columns` (required): An array of objects with the keys label and key. The label is the text to display in the table header and the key is the key for the corresponding data in the data prop.
+
+- `data` (required): An array of objects representing the data to be displayed in the table.
+
+- `pagination` (optional, default `true`): Enables or disables pagination.
+
+- `search` (optional, default `true`): Enables or disables searching.
+
+- `sortable` (optional, default `true`): Enables or disables sorting.
+
+- `filterEntries` (optional, default `true`): Enables or disables filtering the number of entries displayed.
+
+## License
+CustomTable is released under the MIT license. See LICENSE for more information.
